73b05c9f9833f2865c66683e75f6205e66ea8b44
Liviu Ionescu <ilg@livius.net>
2021-09-25 00:04:18 +0300

    defs-source: remove APP_UC_NAME & cleanups

--- scripts/defs-source.sh
+++ scripts/defs-source.sh
@@ -19,16 +19,15 @@
 APP_NAME=${APP_NAME:-"Ninja Build"}
 
 # Used as part of file/folder paths.
-APP_UC_NAME=${APP_UC_NAME:-"Ninja Build"}
 APP_LC_NAME=${APP_LC_NAME:-"ninja-build"}
 
-DISTRO_UC_NAME=${DISTRO_UC_NAME:-"xPack"}
+DISTRO_NAME=${DISTRO_NAME:-"xPack"}
 DISTRO_LC_NAME=${DISTRO_LC_NAME:-"xpack"}
 DISTRO_TOP_FOLDER=${DISTRO_TOP_FOLDER:-"xPacks"}
 
-APP_DESCRIPTION="${DISTRO_UC_NAME} ${APP_UC_NAME}"
+APP_DESCRIPTION="${DISTRO_NAME} ${APP_NAME}"
 
-BRANDING="${BRANDING:-"${DISTRO_UC_NAME} ${APP_NAME}"}"
+BRANDING="${BRANDING:-"${DISTRO_NAME} ${APP_NAME}"}"
 
 # -----------------------------------------------------------------------------
 
