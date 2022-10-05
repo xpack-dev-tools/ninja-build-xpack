aa27f0c94389df42d6a7a6ed88e29c9e15ac3fa1
Liviu Ionescu <ilg@livius.net>
2022-10-05 18:59:58 +0300

    ninja.sh: use stamp

--- scripts/dependencies/ninja.sh
+++ scripts/dependencies/ninja.sh
@@ -33,129 +33,139 @@ function build_ninja()
 
   mkdir -pv "${LOGS_FOLDER_PATH}/${ninja_folder_name}"
 
+  local ninja_stamp_file_path="${STAMPS_FOLDER_PATH}/stamp-${ninja_folder_name}-installed"
+  if [ ! -f "${ninja_stamp_file_path}" ]
+  then
+
   cd "${SOURCES_FOLDER_PATH}"
 
-  if [ ! -d "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" ]
-  then
-    (
-      cd "${SOURCES_FOLDER_PATH}"
-      if [ ! -z ${NINJA_GIT_URL+x} ]
-      then
-        git_clone "${NINJA_GIT_URL}" "${NINJA_GIT_BRANCH}" \
-            "${NINJA_GIT_COMMIT}" "${ninja_src_folder_name}"
-      else
-        download_and_extract "${ninja_github_url}" "${ninja_github_archive}" \
-          "${ninja_src_folder_name}"
-      fi
-      # exit 1
-    )
-  fi
-
-  (
-    mkdir -p "${BUILD_FOLDER_PATH}/${ninja_folder_name}"
-    cd "${BUILD_FOLDER_PATH}/${ninja_folder_name}"
-
-    # xbb_activate_installed_dev
-
-    # CPPFLAGS="${XBB_CPPFLAGS}"
-    CFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
-    CXXFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CXXFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
-    if [ "${TARGET_PLATFORM}" == "win32" ]
-    then
-      CFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
-      CXXFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
-    fi
-    LDFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_LDFLAGS_APP_STATIC_GCC} | sed -e 's|-O[0123s]||')"
-    if [ "${TARGET_PLATFORM}" == "linux" ]
-    then
-      LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
-    fi
-
-    export CFLAGS
-    export CXXFLAGS
-    export LDFLAGS
-
-    local build_type
-    if [ "${IS_DEBUG}" == "y" ]
-    then
-      build_type="Debug"
-    else
-      build_type="Release"
-    fi
-
-    if true # [ ! -f "CMakeCache.txt" ]
+    if [ ! -d "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" ]
     then
       (
-        if [ "${IS_DEVELOP}" == "y" ]
+        cd "${SOURCES_FOLDER_PATH}"
+        if [ ! -z ${NINJA_GIT_URL+x} ]
         then
-          env | sort
+          git_clone "${NINJA_GIT_URL}" "${NINJA_GIT_BRANCH}" \
+              "${NINJA_GIT_COMMIT}" "${ninja_src_folder_name}"
+        else
+          download_and_extract "${ninja_github_url}" "${ninja_github_archive}" \
+            "${ninja_src_folder_name}"
+        fi
+        # exit 1
+      )
+    fi
+
+    (
+      mkdir -p "${BUILD_FOLDER_PATH}/${ninja_folder_name}"
+      cd "${BUILD_FOLDER_PATH}/${ninja_folder_name}"
+
+      # xbb_activate_installed_dev
+
+      # CPPFLAGS="${XBB_CPPFLAGS}"
+      CFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
+      CXXFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CXXFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
+      if [ "${TARGET_PLATFORM}" == "win32" ]
+      then
+        CFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
+        CXXFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
+      fi
+      LDFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_LDFLAGS_APP_STATIC_GCC} | sed -e 's|-O[0123s]||')"
+      if [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+      fi
+
+      export CFLAGS
+      export CXXFLAGS
+      export LDFLAGS
+
+      local build_type
+      if [ "${IS_DEBUG}" == "y" ]
+      then
+        build_type="Debug"
+      else
+        build_type="Release"
+      fi
+
+      if true # [ ! -f "CMakeCache.txt" ]
+      then
+        (
+          if [ "${IS_DEVELOP}" == "y" ]
+          then
+            env | sort
+          fi
+
+          echo
+          echo "Running ninja configure..."
+
+          config_options=()
+
+          # With ninja, the windows build fails with:
+          # The install of the ninja target requires changing an RPATH from the build
+          # tree, but this is not supported with the Ninja generator
+          # config_options+=("-G" "Unix Makefiles")
+          config_options+=("-G" "Ninja")
+
+          config_options+=("-DCMAKE_BUILD_TYPE=${build_type}")
+
+          if [ "${TARGET_PLATFORM}" == "win32" ]
+          then
+              config_options+=("-DWIN32=ON")
+          fi
+
+          if [ "${IS_DEVELOP}" == "y" ]
+          then
+            config_options+=("-DCMAKE_VERBOSE_MAKEFILE=ON")
+          fi
+
+          config_options+=("-DCMAKE_INSTALL_PREFIX=${BINARIES_INSTALL_FOLDER_PATH}")
+
+          run_verbose cmake \
+            "${config_options[@]}" \
+            \
+            "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" \
+
+        ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ninja_folder_name}/cmake-output.txt"
+      fi
+
+      (
+        echo
+        echo "Running ninja build..."
+
+        run_verbose cmake \
+          --build . \
+          --parallel ${JOBS} \
+          --verbose \
+          --config "${build_type}" \
+
+        if [ "${TARGET_PLATFORM}" != "win32" ]
+        then
+          run_verbose ctest -vv
         fi
 
         echo
-        echo "Running ninja configure..."
-
-        config_options=()
-
-        # With ninja, the windows build fails with:
-        # The install of the ninja target requires changing an RPATH from the build
-        # tree, but this is not supported with the Ninja generator
-        # config_options+=("-G" "Unix Makefiles")
-        config_options+=("-G" "Ninja")
-
-        config_options+=("-DCMAKE_BUILD_TYPE=${build_type}")
-
+        # The install target is not functional:
+        mkdir -pv "${BINARIES_INSTALL_FOLDER_PATH}/bin"
         if [ "${TARGET_PLATFORM}" == "win32" ]
         then
-            config_options+=("-DWIN32=ON")
+          install -v -m755 -c ninja.exe "${BINARIES_INSTALL_FOLDER_PATH}/bin"
+        else
+          install -v -m755 -c ninja "${BINARIES_INSTALL_FOLDER_PATH}/bin"
         fi
 
-        if [ "${IS_DEVELOP}" == "y" ]
-        then
-          config_options+=("-DCMAKE_VERBOSE_MAKEFILE=ON")
-        fi
+      ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ninja_folder_name}/build-output.txt"
 
-        config_options+=("-DCMAKE_INSTALL_PREFIX=${BINARIES_INSTALL_FOLDER_PATH}")
+      copy_license \
+        "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" \
+        "${ninja_folder_name}"
 
-        run_verbose cmake \
-          "${config_options[@]}" \
-          \
-          "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" \
+    )
 
-      ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ninja_folder_name}/cmake-output.txt"
-    fi
+    touch "${ninja_stamp_file_path}"
 
-    (
-      echo
-      echo "Running ninja build..."
-
-      run_verbose cmake \
-        --build . \
-        --parallel ${JOBS} \
-        --verbose \
-        --config "${build_type}" \
-
-      if [ "${TARGET_PLATFORM}" != "win32" ]
-      then
-        run_verbose ctest -vv
-      fi
-
-      echo
-      # The install target is not functional:
-      mkdir -pv "${BINARIES_INSTALL_FOLDER_PATH}/bin"
-      if [ "${TARGET_PLATFORM}" == "win32" ]
-      then
-        install -v -m755 -c ninja.exe "${BINARIES_INSTALL_FOLDER_PATH}/bin"
-      else
-        install -v -m755 -c ninja "${BINARIES_INSTALL_FOLDER_PATH}/bin"
-      fi
-
-    ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ninja_folder_name}/build-output.txt"
-
-    copy_license \
-      "${SOURCES_FOLDER_PATH}/${ninja_src_folder_name}" \
-      "${ninja_folder_name}"
-
-  )
+  else
+    echo "Component wine already installed."
+  fi
 
   tests_add "test_ninja" "${BINARIES_INSTALL_FOLDER_PATH}/bin"
 }
