9afd7fbb356c74a18a4c6e68f5d830c8e61f44ea
Liviu Ionescu <ilg@livius.net>
2022-10-06 09:33:42 +0300

    scripts/tests: separate new scripts

---
+++ scripts/tests/run.sh
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+
+function tests_run()
+{
+  echo
+  env | sort
+
+  test_ninja "${TEST_BIN_PATH}"
+}
+
+# -----------------------------------------------------------------------------
