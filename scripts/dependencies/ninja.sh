0809c19e31973a506f91b7e8c51f7607d5d45667
Liviu Ionescu <ilg@livius.net>
2022-12-24 18:19:22 +0200

    ninja.sh: update for XBB_HOST_*

--- scripts/dependencies/ninja.sh
+++ scripts/dependencies/ninja.sh
@@ -64,27 +64,22 @@ function ninja_build()
       mkdir -pv "${XBB_BUILD_FOLDER_PATH}/${ninja_folder_name}"
       cd "${XBB_BUILD_FOLDER_PATH}/${ninja_folder_name}"
 
-      # xbb_activate_installed_dev
+      # xbb_activate_dependencies_dev
 
       # CPPFLAGS="${XBB_CPPFLAGS}"
       CFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
       CXXFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_CXXFLAGS_NO_W} | sed -e 's|-O[0123s]||')"
-      if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
+      if [ "${XBB_HOST_PLATFORM}" == "win32" ]
       then
         CFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
         CXXFLAGS+=" -DUSE_WIN32_CMD_EXE_TO_CREATE_PROCESS"
       fi
 
       # Surprisingly, the Windows archive is longer with static libs, but the
-      # Linux archives are significantly smaller, so static wins.
+      # Linux archives are significantly smaller, so static wins this time.
       LDFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_LDFLAGS_APP_STATIC_GCC} | sed -e 's|-O[0123s]||')"
       # LDFLAGS="$(echo ${XBB_CPPFLAGS} ${XBB_LDFLAGS_APP} | sed -e 's|-O[0123s]||')"
-
-      if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
-      then
-        xbb_activate_cxx_rpath
-        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH:-/non-empty-hack}"
-      fi
+      xbb_adjust_ldflags_rpath
 
       export CFLAGS
       export CXXFLAGS
@@ -110,7 +105,7 @@ function ninja_build()
 
           config_options+=("-DCMAKE_BUILD_TYPE=${build_type}")
 
-          if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
+          if [ "${XBB_HOST_PLATFORM}" == "win32" ]
           then
             # The install of the ninja target requires changing an RPATH from the build
             # tree, but this is not supported with the Ninja generator
@@ -125,9 +120,9 @@ function ninja_build()
             config_options+=("-DCMAKE_VERBOSE_MAKEFILE=ON")
           fi
 
-          config_options+=("-DCMAKE_INSTALL_PREFIX=${XBB_BINARIES_INSTALL_FOLDER_PATH}")
+          config_options+=("-DCMAKE_INSTALL_PREFIX=${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}")
 
-          if [ "${XBB_TARGET_PLATFORM}" == "darwin" ]
+          if [ "${XBB_HOST_PLATFORM}" == "darwin" ]
           then
             # Otherwise it'll generate two -mmacosx-version-min
             config_options+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=${XBB_MACOSX_DEPLOYMENT_TARGET}")
@@ -151,19 +146,19 @@ function ninja_build()
           --verbose \
           --config "${build_type}" \
 
-        if [ "${XBB_TARGET_PLATFORM}" != "win32" ]
+        if [ "${XBB_HOST_PLATFORM}" != "win32" ]
         then
           run_verbose ctest -vv
         fi
 
         echo
         # The install target is not functional:
-        mkdir -pv "${XBB_BINARIES_INSTALL_FOLDER_PATH}/bin"
-        if [ "${XBB_TARGET_PLATFORM}" == "win32" ]
+        mkdir -pv "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/bin"
+        if [ "${XBB_HOST_PLATFORM}" == "win32" ]
         then
-          install -v -m755 -c ninja.exe "${XBB_BINARIES_INSTALL_FOLDER_PATH}/bin"
+          install -v -m755 -c ninja.exe "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/bin"
         else
-          install -v -m755 -c ninja "${XBB_BINARIES_INSTALL_FOLDER_PATH}/bin"
+          install -v -m755 -c ninja "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/bin"
         fi
 
       ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${ninja_folder_name}/build-output-$(ndate).txt"
@@ -178,7 +173,7 @@ function ninja_build()
     touch "${ninja_stamp_file_path}"
 
   else
-    echo "Component ninja already installed."
+    echo "Component ninja already installed"
   fi
 
   tests_add "ninja_test" "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/bin"
@@ -193,16 +188,16 @@ function ninja_test()
   (
     echo
     echo "Checking the ninja shared libraries..."
-    show_libs "${test_bin_path}/ninja"
+    show_host_libs "${test_bin_path}/ninja"
 
     echo
     echo "Checking if ninja starts..."
 
-    run_app "${test_bin_path}/ninja" --version
+    run_host_app_verbose "${test_bin_path}/ninja" --version
 
-    run_app "${test_bin_path}/ninja" --help || true
+    run_host_app_verbose "${test_bin_path}/ninja" --help || true
 
-    run_app "${test_bin_path}/ninja" -t list
+    run_host_app_verbose "${test_bin_path}/ninja" -t list
 
     rm -rf "${XBB_TESTS_FOLDER_PATH}/ninja"
     mkdir -pv "${XBB_TESTS_FOLDER_PATH}/ninja"; cd "${XBB_TESTS_FOLDER_PATH}/ninja"
@@ -217,11 +212,11 @@ __EOF__
 
     touch foo.c
 
-    run_app "${test_bin_path}/ninja" -t targets
-    run_app "${test_bin_path}/ninja" -t rules
-    run_app "${test_bin_path}/ninja" -t commands
+    run_host_app_verbose "${test_bin_path}/ninja" -t targets
+    run_host_app_verbose "${test_bin_path}/ninja" -t rules
+    run_host_app_verbose "${test_bin_path}/ninja" -t commands
 
-    run_app "${test_bin_path}/ninja"
+    run_host_app_verbose "${test_bin_path}/ninja"
   )
 }
 
