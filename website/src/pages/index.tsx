5e53a2e364a61c3c59a4fd11b3480612aced53d1
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:22:13 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @xpack-dev-tools/ninja-build@{customField('releaseVersion')} --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @xpack-dev-tools/ninja-build@1.12.1-1.1 --verbose</InstallWithCopy>
         </div>
 
       </div>
